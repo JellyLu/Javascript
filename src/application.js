ea37e959ea350ae0d3ad9c933fea1d59998be641
Yangjie Lu <634725546@qq.com>
2016-09-11 22:39:07 +0530

    [Yangjie] Added actions for minimum, average, length for buttons, and added tests

--- src/application.js
+++ src/application.js
@@ -8,40 +8,39 @@ var Application = function() {
             };
 
             document.getElementById('max').onclick = function() {
-                var array = document.getElementById("input-box").value.split(",").map(function (number) {
-                    return parseInt(number);
-                });
+                var array = retrieveInputNumbers();
 
                 var max = statisticsCalculator.maximum(array);
                 document.getElementById('result-value').textContent = max;
             };
 
             document.getElementById('min').onclick = function() {
-                var array = document.getElementById("input-box").value.split(",").map(function (number) {
-                    return parseInt(number);
-                });
+                var array = retrieveInputNumbers();
 
                 var min = statisticsCalculator.minimum(array);
                 document.getElementById('result-value').textContent = min;
             };
 
             document.getElementById('average').onclick = function() {
-                var array = document.getElementById("input-box").value.split(",").map(function (number) {
-                    return parseInt(number);
-                });
+                var array = retrieveInputNumbers();
 
                 var average = statisticsCalculator.average(array);
                 document.getElementById('result-value').textContent = average;
             };
 
             document.getElementById('length').onclick = function() {
-                var array = document.getElementById("input-box").value.split(",").map(function (number) {
-                    return parseInt(number);
-                });
+                var array = retrieveInputNumbers();
+
 
                 var length = statisticsCalculator.length(array);
                 document.getElementById('result-value').textContent = length;
             };
+
+            function retrieveInputNumbers() {
+                return document.getElementById("input-box").value.split(",").map(function (number) {
+                    return parseInt(number);
+                });
+            }
         }
     };
 };
